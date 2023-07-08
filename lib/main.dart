204ed8dd54363203bef040f9f7bfe75de541cc30
Trey Hope <trey.a.hope@gmail.com>
2023-07-07 22:49:37 -0500

    6. Create Map in Tiled.io

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,7 @@
-import 'package:flame/game.dart';
 import 'package:flame_audio/flame_audio.dart';
 import 'package:flutter/material.dart';
 import 'package:super_mario_bros/constants/globals.dart';
 import 'package:super_mario_bros/constants/sprite_sheets.dart';
-import 'package:super_mario_bros/games/super_mario_bros_game.dart';
-
-final SuperMarioBrosGame _superMarioBrosGame = SuperMarioBrosGame();
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -26,11 +22,6 @@ void main() async {
   );
 
   runApp(
-    MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: GameWidget(
-        game: _superMarioBrosGame,
-      ),
-    ),
+    MaterialApp(debugShowCheckedModeBanner: false, home: Container()),
   );
 }
