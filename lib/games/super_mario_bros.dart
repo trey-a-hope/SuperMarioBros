0734a01144e22a04100179b20d5d0886ba563282
Trey Hope <trey.a.hope@gmail.com>
2023-07-08 11:13:05 -0500

    8. Use CameraComponent for Viewing Map

--- lib/games/super_mario_bros.dart
+++ lib/games/super_mario_bros.dart
@@ -1,3 +1,4 @@
+import 'package:flame/components.dart';
 import 'package:flame/game.dart';
 import 'package:flame_tiled/flame_tiled.dart';
 import 'package:super_mario_bros/constants/globals.dart';
@@ -5,12 +6,21 @@ import 'package:super_mario_bros/constants/globals.dart';
 class SuperMarioBrosGame extends FlameGame {
   @override
   Future<void> onLoad() async {
+    final World world = World();
+
     final TiledComponent level = await TiledComponent.load(
       Globals.lv_1_1,
       Vector2.all(Globals.tileSize),
     );
 
-    add(level);
+    world.add(level);
+
+    CameraComponent cameraComponent = CameraComponent(world: world)
+      ..viewfinder.visibleGameSize = Vector2(450, 50)
+      ..viewfinder.position = Vector2(0, 0)
+      ..viewfinder.anchor = Anchor.topLeft;
+
+    addAll([cameraComponent, world]);
 
     return super.onLoad();
   }
